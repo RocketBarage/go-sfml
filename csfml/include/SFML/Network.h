97ba1698f38475623dcbe7385a4196aa83bd318f
Schatten <n.kobzev@ittest-team.ru>
2020-03-05 16:53:32 +0300

    add csfml

---
+++ csfml/include/SFML/Network.h
@@ -0,0 +1,44 @@
+////////////////////////////////////////////////////////////
+//
+// SFML - Simple and Fast Multimedia Library
+// Copyright (C) 2007-2015 Laurent Gomila (laurent@sfml-dev.org)
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented;
+//    you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment
+//    in the product documentation would be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such,
+//    and must not be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef SFML_NETWORK_H
+#define SFML_NETWORK_H
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+
+#include "csfml/include/System.h"
+#include "csfml/include/Network/Ftp.h"
+#include "csfml/include/Network/Http.h"
+#include "csfml/include/Network/IpAddress.h"
+#include "csfml/include/Network/Packet.h"
+#include "csfml/include/Network/SocketSelector.h"
+#include "csfml/include/Network/SocketStatus.h"
+#include "csfml/include/Network/TcpListener.h"
+#include "csfml/include/Network/TcpSocket.h"
+#include "csfml/include/Network/UdpSocket.h"
+
+
+#endif // SFML_NETWORK_H
